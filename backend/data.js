4043f66f1f491381f233a800e501518a50db0982
deepu111018 <manognyadeepu@gmail.com>
2022-12-12 22:54:07 +0530

    section-05-Create-Node.js-Server

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'baby frock',
+      slug: 'baby-frock',
+      category: 'frocks',
+      image: '/images/img1.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'white floral print frock',
+      slug: 'white-floral-print-frock',
+      category: 'frocks',
+      image: '/images/img2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'full white frock',
+      slug: 'full-white-frock',
+      category: 'frocks',
+      image: '/images/img3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+    {
+      name: 'green jacket',
+      slug: 'green-jacket',
+      category: 'jackets',
+      image: '/images/img5.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+export default data;
