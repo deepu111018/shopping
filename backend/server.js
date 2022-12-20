43a8731af01a8c65f1ac8b5d4711086a20907981
deepu111018 <manognyadeepu@gmail.com>
2022-12-20 19:26:29 +0530

    section-17-Connect-To-MongoDB Database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+
+dotenv.config();
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
