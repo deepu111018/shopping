dc285e5d94c195f5d314dd5bafdb430a2bef0ccc
deepu111018 <manognyadeepu@gmail.com>
2022-12-24 13:31:04 +0530

    20-Create-Signin-Backend-Api

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,29 @@ import express from 'express';
 
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import seedRouter from './routes/sededRouter.js';
-import productRouter from './routes/productRouter.js';
+import seedRouter from './routes/sededRoutes.js';
+import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
 mongoose.set('strictQuery', false);
+
+const port = process.env.PORT || 5000;
+
 mongoose
   .connect('mongodb://127.0.0.1:27017')
   .then(() => {
@@ -17,12 +34,6 @@ mongoose
     console.log(err.message);
   });
 
-const app = express();
-
-app.use('/api/seed', seedRouter);
-app.use('/api/products', productRouter);
-
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server at http://localhost:${port}`);
 });
