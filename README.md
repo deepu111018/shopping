60c88bda9afb029c1f53056ac3ae380f6072b0ce
deepu111018 <manognyadeepu@gmail.com>
2023-01-15 15:44:12 +0530

    Lesson-24-Implement-Select-Payment-Method-Screen

--- README.md
+++ README.md
@@ -101,3 +101,6 @@
     1.create input forms
     2.handle submit
     3.create backend api
+24. Implement Select Payment Method Screen
+    1.create input forms
+    2.handle submit
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -10,6 +10,10 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+
+    paymentMethod: localStorage.getItem('paymentMethod')
+      ? localStorage.getItem('paymentMethod')
+      : '',
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
@@ -50,6 +54,7 @@ function reducer(state, action) {
         cart: {
           cartItems: [],
           shippingAddress: {},
+          paymentMethod: '',
         },
       };
     case 'SAVE_SHIPPING_ADDRESS':
@@ -59,6 +64,12 @@ function reducer(state, action) {
           ...state.cart,
           shippingAddress: action.payload,
         },
+      };
+
+    case 'SAVE_PAYMENT_METHOD':
+      return {
+        ...state,
+        cart: { ...state.cart, paymentMethod: action.payload },
       };
 
     default:
